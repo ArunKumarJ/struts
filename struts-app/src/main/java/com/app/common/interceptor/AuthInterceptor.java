b3c47e8e362cb7cb34b7c5c536601d57fe3176b4
kondepogupranay@gmail.com <kondepogupranay@gmail.com>
2018-01-08 19:08:45 +0530

    base

---
+++ struts-app/src/main/java/com/app/common/interceptor/AuthInterceptor.java
@@ -0,0 +1,19 @@
+package com.app.common.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class AuthInterceptor extends AbstractInterceptor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3187895812940172508L;
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
